ea051b7ab915cb383d0d43647a49e4ee3f870ce6
junseok <njs702@ajou.ac.kr>
2022-04-25 19:14:06 +0900

    기초적인 CAN 통신 sender 구현 완료

--- CAN1_sender/sender.ino
+++ CAN1_sender/sender.ino
@@ -15,5 +15,17 @@ void setup()
 
 void loop()
 {
-
+    canMsg.can_id = 0x036; // CAN id = 0x036
+    canMsg.can_dlc = 8; // CAN data length = 8
+    canMsg.data[0] = 125; // value in [0] (max=255)
+    canMsg.data[1] = 203; // value in [1] (max=255)
+    canMsg.data[2] = 255; // value in [2] (max=255)
+    canMsg.data[3] = 0x00; // 나머지는 모두 0
+    canMsg.data[4] = 0x00; 
+    canMsg.data[5] = 0x00; 
+    canMsg.data[6] = 0x00; 
+    canMsg.data[7] = 0x00; 
+    
+    mcp2515.sendMessage(&canMsg); // CAN 메시지 전송
+    delay(1000);
 }
