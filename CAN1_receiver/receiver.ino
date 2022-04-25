c942f336815240d5cdacffaf2f78807b4d9feef8
junseok <njs702@ajou.ac.kr>
2022-04-25 19:23:25 +0900

    mcp2515 사용을 위한 변수 선언 및 초기 setup

--- CAN1_receiver/receiver.ino
+++ CAN1_receiver/receiver.ino
@@ -1,9 +1,17 @@
 #include <SPI.h>
 #include <mcp2515.h>
 
+struct can_frame canMsg;
+MCP2515 mcp2515(10); // SPI CS pin 10
+
 void setup()
 {
-	pinMode(LED_BUILTIN,OUTPUT);
+    SPI.begin(); // SPI 통신 시작
+    Serial.begin(9600); // 시리얼 통신 9600 Baud rate
+    mcp2515.reset();
+    // CAN 통신 속도 500KBPS, 클락 속도 8MHZ
+    mcp2515.setBitrate(CAN_500KBPS,MCP_8MHZ);
+    mcp2515.setNormalMode(); // CAN 노말 모드
 }
 
 void loop()
