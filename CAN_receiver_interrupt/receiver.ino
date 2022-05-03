d220160f67653525ec53240d41f0814e18be2ab7
junseok <njs702@ajou.ac.kr>
2022-05-03 18:19:04 +0900

    interrupt 사용해서 1:1로 CAN 통신 하기( 2 Arduino )

---
+++ CAN_receiver_interrupt/receiver.ino
@@ -0,0 +1,39 @@
+#include <SPI.h>
+#include <mcp_can.h>
+
+// spi cs핀 연결 포트 : 10
+MCP_CAN CAN(10);
+
+void setup(){
+    Serial.begin(9600);
+    while(CAN_OK != CAN.begin(CAN_500KBPS,MCP_8MHz)){
+        Serial.println("CAN BUS init Failed");
+        delay(100);
+    }
+    Serial.println("CAN BUS Init Success");
+
+    // 2번 핀 인터럽트 설정(falling 때), 인터럽트 들어오면 CAN_int 함수 실행
+    attachInterrupt(digitalPinToInterrupt(2),CAN_int,FALLING);
+}
+
+void CAN_int(){
+    unsigned char len = 0;
+    unsigned char buf[8];
+
+    CAN.readMsgBuf(&len,buf); // CAN 데이터 가져오기
+    unsigned long cnaId = CAN.getCanId(); // CAN ID 얻기
+    Serial.print("\nData from ID : 0x");
+    Serial.println(cnaId,HEX); // 16진수로 ID 출력
+    for(int i=0;i<len;i++){
+        Serial.print(buf[i]);
+        Serial.print("\t");
+    }
+    Serial.print("\n");
+}
+
+void loop(){
+    Serial.println("Sending");
+    unsigned char stmp[8] = {0,1,2,3,4,5,6,7};
+    CAN.sendMsgBuf(0x11,0,8,stmp); // 데이터 전송
+    delay(1000); // 1.7초마다 전송
+}
